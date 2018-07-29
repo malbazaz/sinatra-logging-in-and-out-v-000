6a4b17602130cda827c91753f7a0b020efb43a61
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-29 14:05:00 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-    #session[:user_id] = @user_id
+    session[:user_id] = @user_id
   if User.is_logged_in?
     @user = User.find(session[:user_id])
   else
