060cf0a1cec7cdd14fd3b581d0579d4d637ea886
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-29 13:38:53 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     binding.pry
-    @user = User.find_by(params[:username])
+    @user = User.find_by_username(params[:username])
     #binding.pry
     if @user
     #  session[:user_id] = @user_id
