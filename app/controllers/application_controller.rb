d9489ea364a06136973556f0734c0b91422bb558
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-29 13:04:36 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-    @user = User.find_by_id(params[:user_id])
+    @user = User.find(session[:user_id]))
     erb :account
   end
 
